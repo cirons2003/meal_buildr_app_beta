5ff8867ee8a63c2abf0f54845b3230bebcd974d3
cirons2003 <ci7229@princeton.edu>
2024-04-23 11:59:39 -0400

    Fixed conditional rendering of user settings link in conversation page

--- client/src/components/ConversationPage/ConversationTopBar.js
+++ client/src/components/ConversationPage/ConversationTopBar.js
@@ -1,14 +1,15 @@
 import { Flex, Text, IconButton } from "@chakra-ui/react";
 import {SettingsIcon} from '@chakra-ui/icons'
 import {Link as RouterLink} from 'react-router-dom'
+import { useTeam } from '../../context'
 
 export default function ConversationTopBar({other_username}) {
-
+    const {team} = useTeam()
     
     return (
         <Flex align = 'center' height = '10%' bg = 'teal' borderRadius = '20px' px = '18px' py = '4px' mb = '15px'>
             <Text fontSize = '28px' as = 'b' color = 'lightblue'>{other_username}</Text>
-            {other_username && <IconButton icon = {<SettingsIcon/>} as = {RouterLink} to = {`../../settings/${other_username}`} bg = 'transparent'></IconButton>}
+            {(team?.role === 'owner' && other_username) && <IconButton icon = {<SettingsIcon/>} as = {RouterLink} to = {`../../settings/${other_username}`} bg = 'transparent'></IconButton>}
         </Flex>
     )
 }
