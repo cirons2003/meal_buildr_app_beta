bd372cef50d5d112a087205c05105df7a7a3021c
cirons2003 <ci7229@princeton.edu>
2024-05-01 16:18:23 -0400

    Further configuration for production

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -13,41 +13,40 @@ from flask_bcrypt import Bcrypt
 
 import boto3
 
-
-app = Flask(__name__) 
-CORS(app, supports_credentials=True)
-
-bcrypt = Bcrypt(app)
-
-#uncomment for testing load_dotenv() 
-app.secret_key = os.getenv('FLASK_SECRET_KEY')
-uri = os.getenv('DATABASE_URI_PRODUCTION') 
-if 'postgresql' in uri or 'postgres' in uri:
-    uri += '?sslmode=require'
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-
-s3_client = boto3.client(
-    's3', 
-    aws_access_key_id = os.getenv('AWS_BUCKET_ACCESS_KEY'),
-    aws_secret_access_key = os.getenv('AWS_BUCKET_SECRET_KEY'),
-    region_name = 'us-east-2'
-)
-
-
-
-from app import models, authentication, views, aws
-
-##returns app instance for deployment
 def create_app():
+
+    app = Flask(__name__) 
+    CORS(app, supports_credentials=True)
+
+    bcrypt = Bcrypt(app)
+
+    #uncomment for testing load_dotenv() 
+    app.secret_key = os.getenv('FLASK_SECRET_KEY')
+    uri = os.getenv('DATABASE_URI_PRODUCTION') 
+    if 'postgresql' in uri or 'postgres' in uri:
+        uri += '?sslmode=require'
+        app.config['SQLALCHEMY_DATABASE_URI'] = uri
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+
+
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+
+    s3_client = boto3.client(
+        's3', 
+        aws_access_key_id = os.getenv('AWS_BUCKET_ACCESS_KEY'),
+        aws_secret_access_key = os.getenv('AWS_BUCKET_SECRET_KEY'),
+        region_name = 'us-east-2'
+    )
+
+
+
+    from app import models, authentication, views, aws
+
     return app
